1685633e38e195faf76213239605a4accda05cf3
gopal-goyal <goyal11.gopal@gmail.com>
2025-01-02 03:16:21 +0530

    added creds for sheets

--- sheets_connection.py
+++ sheets_connection.py
@@ -1,16 +1,35 @@
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
+from firebase_admin import credentials
 import os
 from config import DEV_ENV, SPREADSHEET_ID
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
+# Define the scope for accessing Google Sheets
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 if DEV_ENV:
     cred_path = os.getenv('FIREBASE_CRED_PATH')
-
-# Define the scope for accessing Google Sheets
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+else:
+    # Define the credentials for production using Streamlit secrets
+    firebase_credentials = st.secrets["firebase"]
+    cred = credentials.Certificate({
+        "type": "service_account",
+        "project_id": firebase_credentials["project_id"],
+        "private_key_id": firebase_credentials["private_key_id"],
+        "private_key": firebase_credentials["private_key"],
+        "client_email": firebase_credentials["client_email"],
+        "client_id": firebase_credentials["client_id"],
+        "auth_uri": firebase_credentials["auth_uri"],
+        "token_uri": firebase_credentials["token_uri"],
+        "auth_provider_x509_cert_url": firebase_credentials["auth_provider_x509_cert_url"],
+        "client_x509_cert_url": firebase_credentials["client_x509_cert_url"],
+        "universe_domain": firebase_credentials["universe_domain"]
+    })
+    # Use the certificate for Google Sheets API
+    credentials = Credentials.from_service_account_info(cred, scopes=SCOPES)
 
 # Authenticate with Google Sheets API
 credentials = Credentials.from_service_account_file(cred_path, scopes=SCOPES)
